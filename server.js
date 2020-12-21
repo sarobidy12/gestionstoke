1639d44ead9cfd8d26cd013f073b6b2ae99c7120
sarobidy12 <sarobidyalliance@outlook.com>
2020-12-21 11:21:43 +0300

    v8

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/delete',deleteC);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('front-end/build/')); 
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
          res.sendFile(path.join(__dirname + '/front-end/build/index.html'));
     //      res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'));
     }); 
